bd2bcd68c5ca24b913b5a986501c1eb7c628303a
David Lang <David.lang398@gmail.com>
2025-11-11 15:56:06 +0100

    fix: remove child context api

--- src/views/SceneView.js
+++ src/views/SceneView.js
@@ -17,17 +17,7 @@ type Props = {
 };
 
 export default class SceneView extends PureComponent<void, Props, void> {
-  static childContextTypes = {
-    navigation: propTypes.object.isRequired,
-  };
-
   props: Props;
-
-  getChildContext() {
-    return {
-      navigation: this.props.navigation,
-    };
-  }
 
   render() {
     const { screenProps, navigation, component: Component } = this.props;
